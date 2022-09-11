1232a7fa818e100002acf9aba4db223fc70c219e
Ismail <ismailsulaiman233@gmail.com>
2022-09-11 09:31:35 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,7 @@
 # include <stdio.h>
-int main (void) {puts("\"Programming is like building a multilingual puzzle");return 0;
+/**
+ * main- Entrypoint
+ *  Return: Always 0 (Success)
+ *  */
+int main ()
+{puts("\"Programming is like building a multilingual puzzle");return 0;}
